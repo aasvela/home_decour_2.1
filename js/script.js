7fd94b2bf2fda205dec63fa17ad61b15fc3f64d9
aasvela <73159941+aasvela@users.noreply.github.com>
2023-01-08 23:37:06 +0100

    initial

---
+++ js/script.js
@@ -0,0 +1,34 @@
+  //JavaScript
+  
+  var slideIndex = 1;
+  showSlides(slideIndex);
+
+  function plusSlides(n) {
+    showSlides(slideIndex += n);
+  }
+
+  function currentSlide(n) {
+    showSlides(slideIndex = n);
+  }
+
+  function showSlides(n) {
+    var i;
+    var slides = document.querySelectorAll(".slide");
+    if (n > slides.length) {slideIndex = 1}
+    if (n < 1) {slideIndex = slides.length}
+    for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+    }
+    slides[slideIndex-1].style.display = "block";
+  }
+
+  var prev = document.querySelectorAll(".prev")[0];
+  var next = document.querySelectorAll(".next")[0];
+  prev.addEventListener("click", function() {
+    plusSlides(-1);
+  });
+  next.addEventListener("click", function() {
+    plusSlides(1);
+  });
+
+
